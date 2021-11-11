c63be787792ceaf9a782de4acbf9a6b207f7f221
Joshua B <joshdev@9ci.com>
2021-11-11 16:51:39 -0700

    upgrades gradle and jmh. to make sure we are doing and apples to apples comparison, move classes to main so groovy compileStatic is for sure working. have tests run off same static data, range was one of the many things slowing it down. perf for groovy is on par with java now.

---
+++ src/jmh/groovy/bench/JavaBench.java
@@ -0,0 +1,30 @@
+package bench;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@State(Scope.Benchmark)
+public class JavaBench {
+
+    @Benchmark
+    public void javaForEach() {
+        new JavaLoops().javaForEach();
+    }
+
+    @Benchmark
+    public void javaForLoop() {
+        new JavaLoops().javaForLoop();
+    }
+
+    @Benchmark
+    public void javaIteratorTest() {
+        new JavaLoops().javaIteratorTest();
+    }
+}
--- src/main/groovy/bench/JavaLoops.java
+++ src/main/groovy/bench/JavaLoops.java
@@ -1,35 +1,32 @@
 package bench;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@State(Scope.Benchmark)
-public class JavaBench {
+public class JavaLoops {
 
-    final List<Integer> numbers = IntStream.range(0, 10_000_00)
-            .boxed()
-            .collect(Collectors.toList());
-
-    @Benchmark
     public AtomicLong javaForEach() {
         final AtomicLong result = new AtomicLong();
-        for (int number : numbers) {
+        DataJava.numbers.forEach(number -> {
+            result.addAndGet(number);
+        });
+        return result;
+    }
+
+    public AtomicLong javaForLoop() {
+        final AtomicLong result = new AtomicLong();
+        for (int number : DataJava.numbers) {
             result.addAndGet(number);
         }
         return result;
     }
 
-    @Benchmark
     public AtomicLong javaIteratorTest() {
         final AtomicLong result = new AtomicLong();
-        final Iterator<Integer> iterator = numbers.iterator();
+        final Iterator<Integer> iterator = DataJava.numbers.iterator();
         while (iterator.hasNext()) {
             result.addAndGet(iterator.next());
         }
